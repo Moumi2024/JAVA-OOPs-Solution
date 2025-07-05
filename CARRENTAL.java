7bb9d9782d6190ba5c402f6f44aff1d03f184907
Moumi <byaparimoumi@gmail.com>
2025-07-05 20:14:36 +0530

    Create CARRENTAL.java
    It is a problem solution for the class CARRENTAL in Java. Practices Questions on Class and Objects in Java.

---
+++ CARRENTAL.java
@@ -0,0 +1,40 @@
+import java.util.*;
+
+class CAR {
+    int CarId;
+    String CarType;
+    int Rent;
+
+    public void GetCar() {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter Car Id : ");
+        CarId = sc.nextInt();
+        System.out.println("Enter Car Type : ");
+        CarType = sc.next();
+    }
+
+    public int GetRent() {
+        if (CarType.equals("Small"))
+            Rent = 1000;
+        if (CarType.equals("Van"))
+            Rent = 800;
+        if (CarType.equals("SUV"))
+            Rent = 2500;
+        return Rent;
+    }
+
+    public void ShowCar() {
+        System.out.println("Car Id : " + CarId);
+        System.out.println("Car Type : " + CarType);
+        System.out.println("Rent : " + GetRent());
+    }
+}
+
+class CARRENTAL {
+    public static void main(String args[]) {
+        CAR ob = new CAR();
+        ob.GetCar();
+        ob.GetRent();
+        ob.ShowCar();
+    }
+}
